0800c16bad28cb9c123ade5bd6c137efb268cc39
Anastasia Kurilova <anastas04061999@mail.ru>
2020-05-07 16:50:17 +0700

    Протестировала класс шаблона с типом double, все работает. Тест программы приложила скрином

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -12,10 +12,24 @@ int main() {
 	en.Insert(2);
 	en.Insert(3);
 	en.Insert(2);
+	cout << "int" << endl;
 	en.out();
 	en.remove(4);
-	cout << "AfterRemove" << endl;
+	cout << "After remove" << endl;
 	en.out();
+	
+
+	Planty<double> en2;
+	en2.Insert(5.5);
+	en2.Insert(4.2);
+	en2.Insert(0.000002);
+	en2.Insert(5.6666);
+	cout << "double" << endl;
+	en2.out();
+	en2.remove(4.2);
+	en2.Insert(57.2);
+	cout << "After remove and insert" << endl;
+	en2.out();
 	system("PAUSE");
 
 }
